a311409edb0d31a10b097ac3cc2197762b694087
Salem Ozaki <SOzaki19@cornellcollege.edu>
2019-04-15 17:54:07 -0400

    improved Loan Dao

--- src/main/java/com/st/novatech/springlms/dao/BookLoansDao.java
+++ src/main/java/com/st/novatech/springlms/dao/BookLoansDao.java
@@ -1,9 +1,19 @@
 package com.st.novatech.springlms.dao;
 
-import com.st.novatech.springlms.model.Loan;
+import java.sql.SQLException;
+import java.util.List;
+
+import java.sql.Date;
+
+import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.st.novatech.springlms.model.Loan;
+
 
 /**
  * A Data Access Object interface to access the table of outstanding loans.
@@ -11,7 +21,13 @@ import org.springframework.data.jpa.repository.Query;
  * @author Salem Ozaki
  * @author Jonathan Lovelace
  */
+@Repository
 public interface BookLoansDao extends JpaRepository<Loan, Integer> {
 	@Query(value = "SELECT * FROM tbl_book_loans l WHERE l.bookId = ? AND l.branchId = ? AND l.cardNo = ?", nativeQuery = true)
 	public Loan getLoanByIds(int bookId, int branchId, int cardNo);
+	
+	@Modifying
+	@Query(value = "INSERT INTO tbl_book_loans (bookId, branchId, cardNo, dateOut, dueDate) VALUES (?, ?, ?, ?, ?)", nativeQuery = true)
+	@Transactional
+	public void createLoanWithIds(int bookId, int branchId, int cardNo, Date dateOut, Date dueDate);
 }
